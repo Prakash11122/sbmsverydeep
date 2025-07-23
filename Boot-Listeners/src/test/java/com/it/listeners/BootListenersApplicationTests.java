495d0a3f697a11d986e3f7bf6c0a9c5a691bee7a
prakash kumar pradhan <111282629+Prakash11122@users.noreply.github.com>
2025-07-24 03:20:40 +0530

    Event Listeners

---
+++ Boot-Listeners/src/test/java/com/it/listeners/BootListenersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it.listeners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootListenersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
